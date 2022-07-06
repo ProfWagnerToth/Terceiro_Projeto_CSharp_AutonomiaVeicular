509ea42bef520b4df446ba25a1f40d5663d057d9
Wagner Ramos Toth <103787298+wagnertothetec@users.noreply.github.com>
2022-07-06 17:23:56 -0300

    Projeto para calculo de autonima veicular

--- Terceiro_Projeto_CSharp_AutonomiaVeicular/Form1.cs
+++ Terceiro_Projeto_CSharp_AutonomiaVeicular/Form1.cs
@@ -6,5 +6,23 @@ namespace Terceiro_Projeto_CSharp_AutonomiaVeicular
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Declaração de Variáveis
+            double kmRodado;
+            double litros;
+            double autonomia;
+
+            //Recebendo Valores do Formulário
+            kmRodado = double.Parse(textBox1.Text);
+            litros = double.Parse(textBox2.Text);
+
+            //Calculando autonomia
+            autonomia = kmRodado / litros;
+
+            //Mostrar Valores
+            label5.Text = autonomia.ToString();
+        }
     }
 }
